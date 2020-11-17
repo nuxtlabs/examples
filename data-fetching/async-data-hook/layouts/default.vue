0d27506e519a68c5ccb3e39d83f7daee4db13c2b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-17 14:23:06 +0100

    feat: async data improvements

--- data-fetching/async-data-hook/layouts/default.vue
+++ data-fetching/async-data-hook/layouts/default.vue
@@ -1,11 +1,59 @@
 <template>
   <div>
-    <TheHeader />
     <main>
       <img src="~/assets/logo.svg" />
       <Nuxt />
     </main>
   </div>
 </template>
+<style>
+:root {
+  --primary-color: #00c58e;
+}
 
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  margin: 0;
+}
 
+a,
+a:visited {
+  color: var(--primary-color);
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+main {
+  margin: 0 auto;
+  margin-top: 100px;
+  padding: 0 1rem;
+  max-width: 1280px;
+  text-align: center;
+}
+
+img {
+  margin-bottom: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+
+nav {
+  padding: 0 1rem;
+}
+</style>
--- data-fetching/async-data-hook/nuxt.config.js
+++ data-fetching/async-data-hook/nuxt.config.js
@@ -1,20 +1,6 @@
 export default {
-    /*
-    ** Activate components - https://nuxtjs.org/docs/2.x/directory-structure/components#components-module
-    */
-    components: true,
-
-    /*
-    ** Global CSS - https://nuxtjs.org/docs/2.x/features/configuration#pre-processors
-    */
-    css: [
-        '~/assets/main.css'
-    ], 
-
-    /*
-    ** Modules - https://nuxtjs.org/docs/2.x/directory-structure/modules
-    */
-    modules: [
-        '@nuxt/http',
-    ]  
+  /*
+   ** Modules - https://nuxtjs.org/docs/2.x/directory-structure/modules
+   */
+  modules: ['@nuxt/http']
 }
