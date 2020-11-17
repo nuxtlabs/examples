9acd9cbd0a23b5c91364c498234655dc330115f4
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-17 12:53:49 +0100

    feat: hello world improvements

--- routing/hello-world/pages/about.vue
+++ routing/hello-world/pages/about.vue
@@ -4,6 +4,10 @@
     <p>
       This page is rendered on the <strong>{{ rendering }}</strong>
     </p>
+    <p v-if="rendering === 'server'">
+      First load or hard refresh is done on server side.
+    </p>
+    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
     <ul>
       <li>Refresh the page for server side rendering.</li>
       <li>Click the links to see client side rendering.</li>
