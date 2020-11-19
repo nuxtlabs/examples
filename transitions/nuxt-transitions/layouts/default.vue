9c54bafcf7d488f55ea90d08cb5cc401f3ea0ab3
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-19 19:07:39 +0100

    fix: transitons styles

--- transitions/nuxt-transitions/layouts/default.vue
+++ transitions/nuxt-transitions/layouts/default.vue
@@ -2,18 +2,10 @@
   <div>
     <nav>
       <ul>
-        <li>
-          <NuxtLink to="/">Home</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/fade">Fade</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/bounce">Bounce</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/slide">Slide</NuxtLink>
-        </li>
+        <li> <NuxtLink to="/">Home</NuxtLink> </li>
+        <li> <NuxtLink to="/fade">Fade</NuxtLink> </li>
+        <li> <NuxtLink to="/bounce">Bounce</NuxtLink> </li>
+        <li> <NuxtLink to="/slide">Slide</NuxtLink> </li>
       </ul>
     </nav>
     <main>
@@ -23,49 +15,6 @@
   </div>
 </template>
 <style>
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
-    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-  margin: 0;
-}
-
-nav a {
-  color: inherit;
-  text-decoration: none;
-}
-a.nuxt-link-exact-active {
-  color: #00c58e;
-}
-
-main {
-  margin: 0 auto;
-  margin-top: 100px;
-  padding: 0 1rem;
-  max-width: 1280px;
-  text-align: center;
-}
-
-img {
-  margin-bottom: 1rem;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-li {
-  margin: 0 0.5rem;
-  padding: 0.25rem;
-  font-size: 1.2rem;
-}
-
-nav {
-  padding: 0 1rem;
-}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s;
@@ -123,4 +72,49 @@ nav {
     transform: scale(0);
   }
 }
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  margin: 0;
+}
+
+nav a {
+  color: inherit;
+  text-decoration: none;
+}
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+
+main {
+  margin: 0 auto;
+  margin-top: 100px;
+  padding: 0 1rem;
+  max-width: 1280px;
+  text-align: center;
+}
+
+img {
+  margin-bottom: 1rem;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+
+nav {
+  padding: 0 1rem;
+}
+
 </style>
